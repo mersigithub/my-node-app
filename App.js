ff79ffedc69f1e207ac6498211aac7e5873ff9ba
mersigithub <117305512+mersigithub@users.noreply.github.com>
2025-04-14 10:20:46 -0600

    Update App.js

--- App.js
+++ App.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello CI/CD!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something broke!' });
+  });
